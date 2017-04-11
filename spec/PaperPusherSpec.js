aa5fcb0944a106d620d658e21a8c5724c349c0f0
Avishkar Autar <aautar@digital-radiation.com>
2017-04-11 00:07:16 -0400

    test for unsubscribe and handler call count

--- spec/PaperPusherSpec.js
+++ spec/PaperPusherSpec.js
@@ -1,9 +1,9 @@
 describe("PaperPusher", function() {
   var PaperPusher = require('../src/paper-pusher.js');
-  
-  it("call and passes correct data to handlers", function() {
 
-    jasmine.clock().install();
+  jasmine.clock().install();
+  
+  it("calls and passes correct data to handlers", function() {
     var handlerFunc = jasmine.createSpy('handlerFunc');
 
     var eventSys = new PaperPusher();
@@ -13,7 +13,30 @@ describe("PaperPusher", function() {
     jasmine.clock().tick(1);
 
     expect(handlerFunc).toHaveBeenCalledWith({"data":"blah"});
-
+    expect(handlerFunc.calls.count()).toBe(1);
   });
-  
+
+  it("calls handler once", function() {
+    var handlerFunc = jasmine.createSpy('handlerFunc');
+
+    var eventSys = new PaperPusher();
+    eventSys.subscribe('event', handlerFunc);
+    eventSys.publish('event', {"data":"blah"});
+
+    jasmine.clock().tick(1);
+
+    expect(handlerFunc.calls.count()).toBe(1);
+  });  
+
+  it("does not call handler after unsubscribe", function() {
+    var handlerFunc = jasmine.createSpy('handlerFunc');
+
+    var eventSys = new PaperPusher();
+    eventSys.subscribe('event', handlerFunc);
+    eventSys.unsubscribe('event', handlerFunc);
+    eventSys.publish('event', {"data":"blah"});
+
+    expect(handlerFunc).not.toHaveBeenCalled();
+  });
+
 });
